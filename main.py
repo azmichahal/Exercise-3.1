45a274ad7cc2c72f2bb2b4fb8cc3933a3c2bf497
Azmi Chahal <achahal@enova-me.com>
2024-02-11 21:50:39 +0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create two NumPy arrays
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+# Add the arrays using the + operator
+result = array1 + array2
+
+# Print the result
+print(result)
